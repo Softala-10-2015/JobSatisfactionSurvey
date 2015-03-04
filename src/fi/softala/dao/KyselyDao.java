a93ea3ee546d698e93585e4764dc6fa3bf129b65
Softala2015 <softala2015@gmail.com>
2015-03-04 11:15:22 +0200

    Jes

--- src/fi/softala/dao/KyselyDao.java
+++ src/fi/softala/dao/KyselyDao.java
@@ -1,16 +1,49 @@
 package fi.softala.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+/*
+import Surveybean;
+*/
 
 public class KyselyDao {
-
-	public void HaeKysely(Jokubean x) {
 	
-		//BEAN = kyseky bean tulevaisuudessa
+	@Inject
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
+	
+
+	public List<Survey> HaeKysely(Int i) {
+	
+		//Bean import puuttuu atm
 		
 		String sql = "SELECT *  FROM Survey WHERE survey_id = '?';";
-		RowMapper<Bean> mapper = new BeanRowMapper();
-		List<Bean> Beans = jdbcTemplate.query(sql, mapper);
+		Object[] param = new Object[] { i };
+		RowMapper<Survey> mapper = new SurveyRowMapper();
+		List<Survey> Surveys = jdbcTemplate.query(sql,param, mapper);
 
-	return beans;
+	return Surveys;
 }
 }
