616bb3b733f3a900883a3abbc020de14259392ec
a1300928 <a1300928@pao5009w29.haagahelia.amk>
2015-04-21 10:00:31 +0300

    Uus file

---
+++ src/fi/softala/dao/AnswerTextRowMapper.java
@@ -0,0 +1,22 @@
+package fi.softala.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import fi.softala.bean.Answer;
+
+public class AnswerTextRowMapper implements RowMapper<Answer> {
+	public Answer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Answer answer = new Answer();
+		answer.setAnswerId(rs.getInt("answer_id"));
+		answer.setQuestionId(rs.getInt("question_id"));
+		answer.setAnswerText(rs.getString("answer_text")); 
+		//answer.setaChoiceText(rs.getString("achoice_text"));
+		answer.setQuestionText(rs.getString("question_text"));
+		
+		return answer;
+	}
+
+}
